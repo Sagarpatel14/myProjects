5fd0b17b90224d7d1f6654f1996de89951df564b
sajala soni <sajala.soni1997@gmail.com>
2022-06-29 11:34:17 +0530

    2ndDay

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -3,6 +3,17 @@ const collegeModel = require("../models/collegeModel")
 let createCollege = async function (req, res) {
     try {
         let data = req.body
+
+        if (await collegeModel.findOne({ name: data.name }))
+        return res.status(400).send({ msg: "College name already exist" })
+
+        if (await collegeModel.findOne({ fullName: data.fullName }))
+        return res.status(400).send({ msg: "College fullName already exist" })
+
+        if (await collegeModel.findOne({ logoLink: data.logoLink }))
+        return res.status(400).send({ msg: "logoLink already exist" })
+        
+
         let saveData = await collegeModel.create(data)
         res.status(201).send({ status: true, data: saveData })
     }
--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -3,6 +3,17 @@ const internModel = require("../models/internModel")
 let createIntern = async function(req, res){
     try {
         let data = req.body
+
+        if (await internModel.findOne({ email: data.email }))
+            return res.status(400).send({ msg: "Email Id already exist" })
+
+        if (await internModel.findOne({ mobile: data.mobile }))
+            return res.status(400).send({ msg: "Mobile number already exist" })
+
+        if (!/^\+\d{1,3}-\d{10}$/.test(data.mobile)) {
+            return res.status(400).send({ status: false, message: `Please provide the correct format for mobile number` })
+        }
+
         let saveData = await internModel.create(data)
         res.status(201).send({ status: true, data: saveData })
     }
