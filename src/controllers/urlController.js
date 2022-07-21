aa8e3d036375b4b005efd3825a124e70d50d9a9a
Vikas-Mehra <106535864+Vikas-Mehra@users.noreply.github.com>
2022-07-21 13:03:37 +0530

    Day 4

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,18 +1,21 @@
 const shortid = require("shortid");
+const validator = require("validator");
 const urlModel = require("../models/urlModel");
 
-////!!!!!!!!!!!!!!!!!!!!!!!!!
+//Connect to redis
 const redis = require("redis");
-
 const { promisify } = require("util");
 
-//Connect to redis
 const redisClient = redis.createClient(
-  13190,
-  "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+  // 13190,
+  12992,
+  // "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+  "redis-12992.c301.ap-south-1-1.ec2.cloud.redislabs.com",
   { no_ready_check: true }
 );
-redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+
+// redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+redisClient.auth("Q7uFdUT3ZrBoWYWzf9X019ZIwQy0gTbY", function (err) {
   if (err) throw err;
 });
 
@@ -20,14 +23,8 @@ redisClient.on("connect", async function () {
   console.log("Connected to Redis..");
 });
 
-//1. connect to the server
-//2. use the commands :
-
-//Connection setup for redis
-
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
-////!!!!!!!!!!!!!!!!!!!!!!!!!
 
 //----------------------------------------------------------------------------------------
 //                                1. API -  POST/url/shorten
@@ -35,8 +32,6 @@ const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 const createUrl = async function (req, res) {
   try {
-    console.log("Create URL.");
-
     const body = req.body;
 
     if (Object.keys(body).length === 0) {
@@ -57,23 +52,23 @@ const createUrl = async function (req, res) {
         .send({ status: false, message: "Please enter a valid <longUrl>." });
     }
 
-    if (
-      !/^(https:\/\/www\.|http:\/\/www\.|www\.)[a-zA-Z0-9\-_.$]+\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?/gm.test(
-        longUrl
-      )
-    ) {
+    // !/^(https:\/\/www\.|http:\/\/www\.|www\.)[a-zA-Z0-9\-_.$]+\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?/gm.test(
+    //   longUrl
+    // )
+    if (!validator.isURL(longUrl)) {
       return res
         .status(400)
         .send({ status: false, message: "<longURL> NOT a Valid URL Format." });
     }
 
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     let cachesUrlData = await GET_ASYNC(`${req.body.longUrl}`);
-    // let cachesUrlData = await GET_ASYNC(req.body.longUrl);
+
     if (cachesUrlData) {
-      return res
-        .status(200)
-        .send({ status: true, data: JSON.parse(cachesUrlData) });
+      return res.status(200).send({
+        status: true,
+        message: "URL Data coming from Cache.",
+        data: JSON.parse(cachesUrlData),
+      });
     } else {
       const longUrlUnique = await urlModel.findOne({ longUrl });
       if (longUrlUnique) {
@@ -84,7 +79,7 @@ const createUrl = async function (req, res) {
       }
 
       const urlCode = shortid.generate().toLowerCase();
-      const baseUrl = "localhost:3000";
+      const baseUrl = "http://localhost:3000";
       const shortUrl = baseUrl + "/" + urlCode;
 
       const data = { longUrl, shortUrl, urlCode };
@@ -97,8 +92,7 @@ const createUrl = async function (req, res) {
         urlCode: createData.urlCode,
       };
 
-      await SET_ASYNC(`${req.body}`, JSON.stringify(result));
-      // await SET_ASYNC(req.body, JSON.stringify(result));
+      await SET_ASYNC(`${req.body.longUrl}`, JSON.stringify(result));
 
       return res.status(201).send({
         status: true,
@@ -117,12 +111,9 @@ const createUrl = async function (req, res) {
 
 const getUrl = async function (req, res) {
   try {
-    console.log("GET URL.");
-
     let requestParams = req.params.urlCode;
 
     let cachesUrlData = await GET_ASYNC(`${requestParams}`);
-    // let cachesUrlData = await GET_ASYNC(requestParams);
 
     //convert to object
     const urlData = JSON.parse(cachesUrlData);
@@ -131,12 +122,13 @@ const getUrl = async function (req, res) {
     } else {
       let findUrlCode = await urlModel
         .findOne({ urlCode: requestParams })
-        .select({ urlCode: 1, longUrl: 1, shortUrl: 1 });
+        .select({ urlCode: 1, longUrl: 1, shortUrl: 1, _id: 0 });
 
       if (!findUrlCode) {
-        return res
-          .status(404)
-          .send({ status: false, message: "urlCode NOT Found." });
+        return res.status(404).send({
+          status: false,
+          message: `<urlCode>: <${requestParams}> NOT Found.`,
+        });
       }
 
       await SET_ASYNC(`${requestParams}`, JSON.stringify(findUrlCode));
